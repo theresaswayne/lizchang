db8f178aa7dc842f6661e2af1bce85d9393fd040
Theresa Swayne <theresaswayne@users.noreply.github.com>
2025-10-08 09:05:24 -0400

    Create gaussfittest.r

---
+++ gaussfittest.r
@@ -0,0 +1,45 @@
+# gaussfittest.r
+# demonstration of gaussian fitting using nls
+
+require(tidyverse)
+
+# using random data generated from a normal (Gaussian) density distribution
+x=seq(-4,4,0.01) 
+set.seed(123) # ensure identical results --- change seed to generate different numbers
+# normal distrib with mean of 0.4, sd of 2
+#y=2*dnorm(x, 0.4, 1.5) + runif( length(x) , min = -0.01, max = 0.01)
+y <- dnorm(x, mean=0.4 , sd=2)
+df=data.frame(x,y)
+#nls(y ~ k*dnorm(x, mu,sigma), data = df, start = list(k=2,mu=0.4,sigma=2))
+#df_fit <- nls(y ~ k*dnorm(x, mu,sigma), data = df, start = list(k=1,mu=0,sigma=1))
+df_fit <- nls(y ~ a + (b-a)*exp(-((x-c)^2)/(2*(d^2))), data = df, start = list(a=0, b=0.1, c=0.4, d=2))
+df_mean <- coef(df_fit)[3]
+
+p <- coef(df_fit)
+plot(df$x, df$y)
+curve(p["a"] + (p["b"]-p["a"])*exp(-((x-p["c"])^2)/(2*(p["d"]^2))), from = -4, to = 4, lwd=2, col="Red", add=TRUE)
+
+# using real data
+
+gp <- read_csv("Hapi/test output/otsu-2025926(17h06)/Masked (by Sum of ordered + disordered) GP images/Histograms/J774-CL90min-20uM-2chs.nd2GP Histogram(masked by Sum of ordered + disordered).csv")
+
+gpdf <- data.frame(x = gp$`GP values`, y = gp$`Counts (Pixels)`)
+# guess at the vmean and other params
+mu_guess <- gpdf$x[which.max(gpdf$y)]
+a_guess <- 13000
+b_guess <- 300000
+d_guess <- 0.07
+#gp_fit <- nls(y ~ k*dnorm(x, mu,sigma), data = gpdf, start = list(k=1,mu=0,sigma=0.5))
+# d is sqrt of variance, I think
+gp_fit <- nls(y ~ a + (b-a)*exp(-((x-c)^2)/(2*(d^2))), data = gpdf, start = list(a=a_guess, b=b_guess, c=mu_guess, d=d_guess))
+#gp_coeffs <- coef(gp_fit)
+#gp_mean <- gp_coeffs[4]
+
+# plot the fitted function in red, guessed function in blue, and the raw data
+q <- coef(gp_fit)
+plot(gpdf$x, gpdf$y)
+curve(q["a"] + (q["b"]-q["a"])*exp(-((x-q["c"])^2)/(2*(q["d"]^2))), from = -1, to = 1, lwd=2, col="Red", add=TRUE)
+curve(a_guess + (b_guess-a_guess)*exp(-((x-mu_guess)^2)/(2*(d_guess^2))), from = -1, to = 1, lwd=2, col="Blue", add=TRUE)
+# retrieve the estimated mean (c) and its standard error
+gp_mean <- summary(gp_fit)$coefficients[3,1]
+gp_se <- summary(gp_fit)$coefficients[3,2]
